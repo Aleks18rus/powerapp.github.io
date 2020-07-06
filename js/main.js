5aeb085d415d7d8737ea3dcce1aa335fe187c892
Aleks18rus <58142614+Aleks18rus@users.noreply.github.com>
2020-07-06 21:18:27 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,91 @@
+/* Плавная прокрутка */
+let anchors = document.querySelectorAll("header a[href*='#']");
+
+for (anchors of anchors) {
+  if (anchors) {
+    anchors.addEventListener("click", function (e) {
+      e.preventDefault();
+      anchorId = this.getAttribute("href");
+      document.querySelector(anchorId).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
+}
+
+/* Паралакс */
+function parallax(e) {
+  this.querySelectorAll(".map .iphone").forEach((parallaxImg) => {
+    parallaxImg.style.transform = `translateX(${e.clientX})px`;
+  });
+}
+
+document.addEventListener("mousemove", parallax);
+
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: "aos-init", // class applied after initialization
+  animatedClassName: "aos-animate", // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
+
+
+new WOW().init();
+
+document.querySelector('.map').onmousemove = (e) => {
+
+  const X = e.clientX * 0.01;
+  const X2 = e.clientX * 0.005;
+
+  document.querySelector('.iphone img').style.transform = `translateX(-${X}px)`;
+  document.querySelector('.bg img').style.transform = `translateX(${X2}px)`;
+}
+
+const mobile_img = document.querySelector('#mobile_img');
+const power_img = document.querySelector('#power_img');
+
+let cord = 0,
+    current = 0;
+window.onscroll = () => {
+
+  setTimeout(() => {
+    current = pageYOffset;
+  }, 100);
+
+  if(pageYOffset > 1000 && pageYOffset < 1500)
+  {
+    if(pageYOffset > current)
+      cord+=3;
+    else
+      cord-=3;
+
+    mobile_img.style.transform = `translateY(${cord}px)`;
+    power_img.style.transform = `translateY(-${cord}px)`;
+
+  }
+}
+
+
+//Partners
+
+// document.querySelector('.super-action').onmousemove = (e) => {
+
+//   const Y = e.clientY * 0.05;
+
+//   document.querySelector('.left img').style.transform = `translateY(-${Y}px)`;
+// }
